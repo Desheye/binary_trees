2f397515db37b958bc3dff11b7887dfa46f7a5af
Desheye <aarealaafiaboyz@gmail.com>
2024-02-27 15:43:52 +0100

    fixed again

--- binary_trees.h
+++ binary_trees.h
@@ -21,7 +21,7 @@ struct binary_tree_s
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-}/* binary_tree_t*/;
+}binary_tree_t;
 
 void binary_tree_print(const binary_tree_t *);
 
