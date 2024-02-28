dadfb03cc0951986733046b5ca4583b8235bf799
Desheye <aarealaafiaboyz@gmail.com>
2024-02-29 00:42:21 +0100

    betty to the rescue

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -8,16 +8,15 @@
  */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree == NULL || func == NULL)
-        return;
+	if (tree == NULL || func == NULL)
+		return;
 
-    /* Recursively traverse the left subtree */
-    binary_tree_inorder(tree->left, func);
+	/* Recursively traverse the left subtree */
+	binary_tree_inorder(tree->left, func);
 
-    /* Call the function with the value of the current node */
-    func(tree->n);
+	/* Call the function with the value of the current node */
+	func(tree->n);
 
-    /* Recursively traverse the right subtree */
-    binary_tree_inorder(tree->right, func);
+	/* Recursively traverse the right subtree */
+	binary_tree_inorder(tree->right, func);
 }
-
