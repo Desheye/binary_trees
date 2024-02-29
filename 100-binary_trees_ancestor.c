2c6687ea092ba4d2c134d44002b1eaa3fa480379
Desheye <aarealaafiaboyz@gmail.com>
2024-02-29 01:37:18 +0100

    betty for the road

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,5 +1,3 @@
-#include "binary_trees.h"
-
 /**
  * binary_trees_ancestor - Finds the lowest common ancestor of two nodes.
  * @first: A pointer to the first node.
@@ -8,21 +6,21 @@
  * Return: A pointer to the lowest common ancestor node,
  *         or NULL if no common ancestor was found.
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+                                     const binary_tree_t *second)
 {
-    const binary_tree_t *temp1 = first, *temp2 = second;
+	const binary_tree_t *temp1 = first, *temp2 = second;
 
-    while (temp1)
-    {
-        temp2 = second;
-        while (temp2)
-        {
-            if (temp1 == temp2)
-                return ((binary_tree_t *)temp1);
-            temp2 = temp2->parent;
-        }
-        temp1 = temp1->parent;
-    }
-    return (NULL);
+	while (temp1)
+	{
+		temp2 = second;
+		while (temp2)
+		{
+			if (temp1 == temp2)
+				return ((binary_tree_t *)temp1);
+			temp2 = temp2->parent;
+		}
+		temp1 = temp1->parent;
+	}
+	return (NULL);
 }
-
--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,24 +8,21 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-    int left_height, right_height;
+	if (tree == NULL)
+		return (0);
 
-    if (tree == NULL)
-        return (0);
+	int left_height = binary_tree_height(tree->left);
+	int right_height = binary_tree_height(tree->right);
 
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right);
+	if (left_height == right_height)
+	{
+		if (tree->left == NULL && tree->right == NULL)
+			return (1);
 
-    if (left_height == right_height)
-    {
-        if (tree->left == NULL && tree->right == NULL)
-            return (1);
+		if (binary_tree_is_perfect(tree->left) &&
+			binary_tree_is_perfect(tree->right))
+			return (1);
+	}
 
-        if (binary_tree_is_perfect(tree->left) &&
-            binary_tree_is_perfect(tree->right))
-            return (1);
-    }
-
-    return (0);
+	return (0);
 }
-
