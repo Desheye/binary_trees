b055a199b84cfd8b56f87bc8ff4618e881e52662
Desheye <aarealaafiaboyz@gmail.com>
2024-03-01 15:47:42 +0100

    fixed 9

--- 133-heap_extract.c
+++ 133-heap_extract.c
@@ -1,61 +1,30 @@
 #include "binary_trees.h"
-#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * heap_extract - Extracts the root node of a Max Binary Heap
- * @root: A double pointer to the root node of the heap
+ * heap_to_sorted_array - Converts a Binary Max Heap to a sorted array
+ *                        of integers.
+ * @heap: Pointer to the root node of the heap to convert.
+ * @size: Pointer to store the size of the array.
  *
- * Return: The value stored in the root node, or 0 on failure
+ * Return: Pointer to the sorted array of integers
+ * (sorted in descending order).
  */
-int heap_extract(heap_t **root)
+int *heap_to_sorted_array(heap_t *heap, size_t *size)
 {
-heap_t *last_node, *tmp;
-int value;
-size_t size, height, i;
-heap_t **array;
+int *sorted_array;
+size_t i;
+if (!heap || !size)
+return (NULL);
 
-if (root == NULL || *root == NULL)
-	return (0);
+*size = binary_tree_size(heap);
+sorted_array = malloc(*size * sizeof(int));
+if (!sorted_array)
+return (NULL);
 
-value = (*root)->n;
-if ((*root)->left == NULL && (*root)->right == NULL)
-{
-    free(*root);
-    *root = NULL;
-    return (value);
-}
+    /* Convert heap to sorted array */
+for (i = 0; i < *size; i++)
+	sorted_array[i] = heap_extract(&heap);
 
-height = binary_tree_height(*root);
-size = binary_tree_size(*root);
-array = malloc(sizeof(heap_t *) * size);
-if (array == NULL)
-    return (0);
-
-for (i = 0; i < size; i++)
-array[i] = NULL;
-i = 0;
-array[i] = *root;
-while (array[i] != NULL)
-{
-if (array[i]->left != NULL)
-{
-array[i * 2 + 1] = array[i]->left;
-if (array[i * 2 + 1]->n > array[i]->n)
-                tmp = array[i];
-        }
-        if (array[i]->right != NULL)
-        {
-            array[i * 2 + 2] = array[i]->right;
-            if (array[i * 2 + 2]->n > array[i]->n)
-                tmp = array[i];
-        }
-        i++;
-    }
-    last_node = array[i - 1];
-    free(*root);
-    *root = last_node;
-
-    if (tmp == NULL)
-        return (value);
-    return (value);
+return (sorted_array);
 }
